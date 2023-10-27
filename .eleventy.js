f38312a04507d6322dd18dc3510493008d1c358c
Manuel Rego Casasnovas <rego@igalia.com>
2023-10-27 12:38:46 +0200

    Update favicon

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const MarkdownIt = require("markdown-it");
 module.exports = function(eleventyConfig) {
   eleventyConfig.setTemplateFormats(["md", "njk", "html", "css", "pdf"]);
 
+  eleventyConfig.addPassthroughCopy({"_assets/img/favicon.ico": "/" });
   eleventyConfig.addPassthroughCopy({"_assets/img": "img"});
   eleventyConfig.addPassthroughCopy({"_assets/css": "css"});
   eleventyConfig.addPassthroughCopy({"previous/": "/"});
